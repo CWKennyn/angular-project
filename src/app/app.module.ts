f176949c8ab4696fd510f97d8307d754a46243db
Kenny Nguyen <kennynguyen@codeweavers.net>
2023-12-18 16:04:26 +0000

    Understanding root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
+  declarations: [
     AppComponent
   ],
-  declarations: [
-  ]
+  imports: [
+    BrowserModule,
+    CommonModule
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
